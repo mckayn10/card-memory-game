b724edcda154aed08ab1ce77632c39b5d1a33175
mckaynilsson10 <nilsson@foreup.com>
2021-07-28 16:32:19 -0600

    formatting and gh-pages additions

--- src/views/GameOver/GaveOverView.js
+++ src/views/GameOver/GaveOverView.js
@@ -10,7 +10,6 @@ class GameOverView extends Component {
             <div className="game-over-container">
                 <h1 className="game-over-text-lg">GAME OVER</h1>
                 <h3 className="game-over-text">You finished the game in {this.props.clicks} clicks!</h3>
-                <h3 className="game-over-text">Can you do better?</h3>
                 <button onClick={this.props.restart} className="play-again-btn">Play Again</button>
             </div>
         )
